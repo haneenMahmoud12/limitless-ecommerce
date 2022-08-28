25491808ab896ac46345eb9d41b08862313b93c9
Haneen Mahmoud <haneenmahmoud37@gmail.com>
2022-08-28 12:29:26 +0200

    second commit

---
+++ src/app/offers-carousel/offers-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-offers-carousel',
+  templateUrl: './offers-carousel.component.html',
+  styleUrls: ['./offers-carousel.component.css']
+})
+export class OffersCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
